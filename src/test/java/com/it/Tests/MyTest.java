87ae621de66528bb840edd84f15b3b0c085058b6
Marinellalala <marina.pyrozhenko@gmail.com>
2019-11-15 19:59:13 +0200

    second lesson

--- src/test/java/com/it/Tests/MyTest.java
+++ src/test/java/com/it/Tests/MyTest.java
@@ -1,13 +1,17 @@
 package com.it.Tests;
 
+import com.it.Users.UserFactory;
+import com.it.Utils.Utils;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class MyTest extends BaseTest{
     @Test
     public void test1() throws InterruptedException {
-        app.login.login("ittest2", "337774a");
+        System.out.println(UserFactory.getRandomUser());
+        System.out.println(UserFactory.getRandomUsers(20));
+        app.login.login(validUser);
         Thread.sleep(2000);
-        Assert.assertEquals(app.dashboard.getEmailUser(), "ittest2@i.ua");
+        Assert.assertEquals(app.dashboard.getEmailUser(), validUser.email);
     }
 }
